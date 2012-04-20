15c51a0b4170c26836a2f305c253eb30f58b6175
Ilias Stamatis <stamatis.iliass@gmail.com>
2012-04-20 19:29:37 +0300

    minor change on HELP_MSG

--- info.h
+++ info.h
@@ -16,18 +16,18 @@
     "There are trees, bushes, flowers, grass, and fruits all around you." \
     "\n\nWhat a wonderful place!\nBut beware! You're not alone!\n"
 
-#define HELP_MSG \
-"Only the following commands are understood by the program:\n\n"                      \
-"  Command             Description\n"                                                 \
-" -------------------------------------------------------------------------\n"        \
-"  shoot <id>          shoots the animal with the corresponding id\n"                 \
-"  look                displays info about all animals on the scene\n"                \
-"  status              displays info about player\n"                                  \
-"  buy <type> <spef>   buys the requested stuff\n"                                    \
-"                         examples: buy bullets 5, buy drugs 1, buy weapon rifle\n" \
-"  help, ? <table>     if called without parameter, prints this help text\n"          \
-"                         you can user either \"help animals\" or \"help weapons\"\n" \
-"                         to print a table of all animals or weapons\n"               \
-"  exit, quit          exits the program\n"                                           \
+#define HELP_MSG                                                                          \
+    "Only the following commands are understood by the program:\n\n"                      \
+    "  Command             Description\n"                                                 \
+    " -------------------------------------------------------------------------\n"        \
+    "  shoot <id>          shoots the animal with the corresponding id\n"                 \
+    "  look                displays info about all animals on the scene\n"                \
+    "  status              displays info about player\n"                                  \
+    "  buy <type> <spef>   buys the requested stuff\n"                                    \
+    "                         examples: buy bullets 5, buy drugs 1, buy weapon rifle\n"   \
+    "  help, ? <table>     if called without parameter, prints this help text\n"          \
+    "                         you can user either \"help animals\" or \"help weapons\"\n" \
+    "                         to print a table of all animals or weapons\n"               \
+    "  exit, quit          exits the program\n"                                           
 
 #endif
